f43efdb0b2fba2f5ab3c124184e398f0969d93f6
one10 <ivan.balepin@gmail.com>
2018-11-09 12:10:03 -0800

    bit of error handling never hurt nobody

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const driver = require('./driver');
 const {WikipediaSearch} = require('./driver/wikipediaSearch');
-const {BusinessSearchSosCaGov} = require('./driver/otherPages');
 
 // Note: Wikipedia is only an example here. It's pointless to try to scrape it, all the data is downloadable!
-//driver.run(BusinessSearchSosCaGov);
 driver.run(WikipediaSearch);
